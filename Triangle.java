25f49dc634a94a67ae82aa873857c053f9b36911
Bilal Ahmed <74753793+bilalahmed23070@users.noreply.github.com>
2022-11-14 21:55:11 +0500

    Add files via upload

---
+++ Triangle.java
@@ -0,0 +1,50 @@
+/*  BILAL AHMED
+    ERP: 23070
+ */
+public class Triangle {
+    private Point vertexA;
+    private Point vertexB;
+    private Point vertexC;
+    private Line medianA;
+    private Line medianB;
+    private Line medianC;
+
+
+    public Triangle(Point a, Point b, Point c){
+    vertexA = a;
+    vertexB = b;
+    vertexC = c;
+    Point temp1 = new Point((vertexB.getX()+vertexC.getX())/2,(vertexB.getY()+vertexC.getY())/2);
+    medianA = new Line(vertexA,temp1);
+    Point temp2 = new Point((vertexC.getX()+vertexA.getX())/2,(vertexC.getY()+vertexA.getY())/2);
+    medianB = new Line(vertexB,temp2);
+    Point temp3 = new Point((vertexA.getX()+vertexB.getX())/2,(vertexA.getY()+vertexB.getY())/2);
+    medianC = new Line(vertexC,temp3);
+    }
+
+    public String toString(){
+        return "VertexA:("+vertexA.getX()+","+vertexA.getY()+") VertexB:("+vertexB.getX()+","+vertexB.getY()+") VertexC:("+vertexC.getX()+","+vertexC.getY()+")";
+    }
+
+    public double getPerimeter(){
+        double dist1 = Math.sqrt(((vertexB.getX()- vertexA.getX())*(vertexB.getX()- vertexA.getX()))+((vertexB.getY()- vertexA.getY())*(vertexB.getY()- vertexA.getY())));
+        double dist2 = Math.sqrt(((vertexC.getX()- vertexB.getX())*(vertexC.getX()- vertexB.getX()))+((vertexC.getY()- vertexB.getY())*(vertexC.getY()- vertexB.getY())));
+        double dist3 = Math.sqrt(((vertexA.getX()- vertexC.getX())*(vertexA.getX()- vertexC.getX()))+((vertexA.getY()- vertexC.getY())*(vertexA.getY()- vertexC.getY())));
+        return dist1+dist2+dist3;
+
+    }
+
+    public String getMedianA(){
+        return "The coordinates of MedianA are "+this.medianA.toString()+" And the length is "+ this.medianA.getLength();
+    }
+
+    public String getMedianB(){
+        return "The coordinates of MedianB are "+this.medianB.toString()+" And the length is "+ this.medianB.getLength();
+    }
+
+    public String getMedianC(){
+        return "The coordinates of MedianC are "+this.medianC.toString()+" And the length is "+ this.medianC.getLength();
+    }
+
+
+}
